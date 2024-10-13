9b63a8cdaa34005d1dd373f13e02da939a6a0c79
wipesides <atasesli@protonmail.com>
2024-10-13 21:41:31 +0300

    Implementing firebase functions, to be continued

--- app/firebase/auth.ts
+++ app/firebase/auth.ts
@@ -1,6 +1,39 @@
-import {auth} from './firebase.config';
+import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
+import { getFirestore, doc, setDoc, collection, addDoc} from "firebase/firestore";
 
 export async function login(email: string,password:string){
-    
+    const auth = getAuth();
+    const response = await signInWithEmailAndPassword(auth,email,password);
 }
-export async function
+export async function register(name: string, email: string, password: string){
+    try {
+        const auth = getAuth();
+        const authResponse = await createUserWithEmailAndPassword(auth,email,password);
+        const firestore = getFirestore();
+        const db = collection(firestore,"users");
+        const userRef = doc(db,authResponse.user.uid);
+        const userData = {
+            id: authResponse.user.uid,
+            name: name,
+            email: email,
+            highestScore: "0"
+        }
+        await setDoc(userRef,userData);
+    }
+    catch (e: unknown){
+        console.error("An error occured during registration: ",e);
+        if (e instanceof Error) {
+            alert(e.message || "An error occured during registration. Try again");
+        } else {
+            alert("An error occured during registration. Try again");
+        }
+    }
+    const firestore = getFirestore();
+    const userData = {
+        name: name,
+        email: email,
+        highestScore: "0"
+    }
+    const db = collection(firestore,"users");
+    const docRef = await addDoc(db,userData);
+}
--- app/firebase/firestore.ts
+++ app/firebase/firestore.ts
@@ -1,1 +1,21 @@
-import {firestore} from './firebase.config';
+import { getAuth } from "firebase/auth";
+import { arrayUnion, collection, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc} from "firebase/firestore";
+
+export async function getScoreBoard(){
+    const firestore = getFirestore();
+    const db = collection(firestore,"users");
+    const scoresSnapshot = await getDocs(db);
+    const scoresList = scoresSnapshot.docs.map(doc => doc.data());
+}
+export async function updateHighestScore(currentScore: string){
+    const auth = getAuth();
+    const userRef = auth.currentUser!.uid;
+    const firestore = getFirestore();
+    const db = collection(firestore,"users");
+    const userDocRef = doc(db,userRef);
+    const userDocRaw = await getDoc(userDocRef);
+    const userData = userDocRaw.data();
+    if (userData!["highestScore"] > 0){
+        updateDoc(userDocRef,{"highestScore": arrayUnion(currentScore)});
+    }
+}
